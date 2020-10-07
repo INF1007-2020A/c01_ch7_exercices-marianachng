9f8b360139d8eef994b5990aa04adbeed1250351
Mariana Chang Ieong <mariana.chang-ieong@polymtl.ca>
2020-10-07 15:47:55 -0400

    MCI: ex 1 volume et masse ellipsoide

--- exercice.py
+++ exercice.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import turtle 
+from math import pi
 
 # TODO: Importez vos modules ici
+def ellipse(masse_volumique: float, a: float=1, b: float=2, cF float=3) -> tuple:
+    volume = 4/3 * pi * a * b * c
+    mass = volume * masse_volumique
 
+    return volume, mass
 
 
 # TODO: Définissez vos fonction ici
@@ -12,4 +17,5 @@ import turtle
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
 
-    pass
+    volume, masse = ellipse(10, 2, 4)
+    print("le volume est", volume, "et sa masse est", masse, ".")
