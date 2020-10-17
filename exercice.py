eee2a193da245c8f130bdfa5711f971dd00df93d
Mariana Chang Ieong <mariana.chang-ieong@polymtl.ca>
2020-10-16 22:13:19 -0400

    MCI: functions defined

--- exercice.py
+++ exercice.py
@@ -1,18 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# TODO: Importez vos modules ici
+
 import turtle 
 from math import pi
 
-# TODO: Importez vos modules ici
-def ellipse(masse_volumique: float, a: float=1, b: float=2, cF float=3) -> tuple:
+# TODO: Définissez vos fonction ici
+
+def ellipse(masse_volumique: float, a: float=1, b: float=2, c: float=3) -> tuple:
     volume = 4/3 * pi * a * b * c
     mass = volume * masse_volumique
 
     return volume, mass
 
+def sort_letters():
+    pass
 
-# TODO: Définissez vos fonction ici
+def draw_tree():
+    pass
 
+def valid_chain():
+    pass
+
+def dna(chaine:str):
+    pass
+
+def proportion_dna(chain, sequence):
+    pass
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
