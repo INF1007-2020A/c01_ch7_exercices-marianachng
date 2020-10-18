a431fe1895d593afef45602b730f57eaaeb6c361
Mariana Chang Ieong <mariana.chang-ieong@polymtl.ca>
2020-10-18 17:57:58 -0400

    MCI: draw_tree still needs recursion

--- exercice.py
+++ exercice.py
@@ -15,10 +15,6 @@ def ellipse(masse_volumique: float, a: float=1, b: float=2, c: float=3) -> tuple
 
 def sort_letters():
     pass
-
-def draw_tree():
-    pass
-
 def valid_chain():
     pass
 
@@ -28,8 +24,62 @@ def dna(chaine:str):
 def proportion_dna(chain, sequence):
     pass
 
+#PRACTICE TURTLE:
+def draw_square():
+
+    square = turtle.Turtle()
+
+    square.forward(100)
+    square.left(90)
+    square.forward(100)
+    square.left(90)
+    square.forward(100)
+    square.left(90)
+    square.forward(100)
+    
+def draw_branch(size):
+
+    shrink_factor = 0.8
+    angle = 30
+    branch.forward(size)
+    branch.right(angle)
+    branch.forward(size*shrink_factor)
+    branch.backward(size*shrink_factor)
+    branch.left(2*angle)
+    branch.forward(size*shrink_factor)
+    branch.backward(size*shrink_factor)
+    branch.right(angle)
+    branch.backward(size)
+
+def draw_tree(size, depth):
+    branch_count = 1
+    shrink_factor = 0.8
+    angle = 30
+    depth = 0
+
+    if depth >= branch_count:
+        pass
+    else:
+        branch.forward(size)
+        branch.right(angle)
+        branch.forward(size*shrink_factor)
+        branch.left(2*angle)
+        branch.forward(size*shrink_factor)
+        draw_tree(size*shrink_factor, depth+1)
+    
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
 
-    volume, masse = ellipse(10, 2, 4)
-    print("le volume est", volume, "et sa masse est", masse, ".")
+    #volume, masse = ellipse(10, 2, 4)
+    #print("le volume est", volume, "et sa masse est", masse, ".")
+    
+    #print(draw_square())
+    #branch = turtle.Turtle()
+    #branch.setheading(90)
+    #branch.color("green")
+    #branch.width(4)
+    #branch.speed(1)
+#
+    #draw_tree(100,3)
+    #turtle.done()
+    
